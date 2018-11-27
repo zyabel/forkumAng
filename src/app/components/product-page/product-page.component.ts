568d0e36e2c66ac6e86f14e477f95c0be09152c1
Yurii Ziablia <yziab@softserveinc.com>
2018-11-27 18:27:08 +0200

    Created basic structure of the products page.

--- src/app/components/product-page/product-page.component.ts
+++ src/app/components/product-page/product-page.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+
+import { ProductCard } from '../../interfaces/news.interface';
+
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'app-product-page',
@@ -6,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent implements OnInit {
+  productsArray: ProductCard[];
 
-  constructor() { }
+  productCard: ProductCard;
 
-  ngOnInit() {
+  contentArray = new Array(90).fill('');
+  returnedArray: string[];
+
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
+
+  ngOnInit(): void {
+    this.contentArray = this.contentArray.map((v: string, i: number) => `Content line ${i + 1}`);
+    this.returnedArray = this.contentArray.slice(0, 10);
   }
 
+  pageChanged(event: PageChangedEvent): void {
+    const startItem = (event.page - 1) * event.itemsPerPage;
+    const endItem = event.page * event.itemsPerPage;
+    this.returnedArray = this.contentArray.slice(startItem, endItem);
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
--- src/app/components/product-page/product-page.module.ts
+++ src/app/components/product-page/product-page.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { ProductPageComponent } from './product-page.component';
 import { ProductPageRoutingModule } from './product-page.routing.module';
 
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+
 @NgModule({
   declarations: [
     ProductPageComponent
   ],
   imports: [
     CommonModule,
-    ProductPageRoutingModule
+    ProductPageRoutingModule,
+    PaginationModule.forRoot(),
+    ModalModule.forRoot()
   ],
   exports: [
     ProductPageComponent
