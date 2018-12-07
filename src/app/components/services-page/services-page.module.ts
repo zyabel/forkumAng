3141ae7cf029aae795a9cee9af2591284eef1c5c
Yurii Ziablia <yziab@softserveinc.com>
2018-12-07 17:04:44 +0200

    Added text information for the service and faq pages.

--- src/app/components/services-page/services-page.module.ts
+++ src/app/components/services-page/services-page.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ServicesPageComponent } from './services-page.component';
 import { ServicesPageRoutingModule } from './services-page.routing.module';
 
+import { TabsModule } from 'ngx-bootstrap';
+
 @NgModule({
   declarations: [
     ServicesPageComponent
   ],
   imports: [
     CommonModule,
+    TabsModule.forRoot(),
     ServicesPageRoutingModule
   ],
   exports: [
